d789c9ec2a15b75984bc41ce57b6f254365610ef
nagizo <naginag0605@gmail.com>
2022-07-02 04:15:30 +0900

    ‘update’

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './App.css';
+import SearchIcon from '@mui/icons-material/Search';
+import WeatherScreen from '../src/components/WeatherScreen';
+
 
 
 
 const App = () => {
 
-  const [lat, setLat] = useState([]);
-  const [lon, setLon] = useState([]);
-  const [currentData, setCurrentData] = useState([]);
+  const [ Available, setAvailable ] = useState(true);
+  const [ positon, setPositon ] = useState({ latitude: null, longitude: null })
 
-  const API_KEY = 'f7927c318eebf0ac033c6981f4624d57'
-  const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-
+  //useEffectが実行されているか判定
+  const FirstRef = useRef(true);
+  
+  //レンダリング時にGeolocationAPIが使えるか判定する
   useEffect(() => {
-    const Data =  async () => {}
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
-      console.log(lat,lon)
-  });
-    fetch(currentURL)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data)
-        setCurrentData(currentData)
-      });
-    Data();
-  }, [lat, lon])
+    FirstRef.current = false;
+    if ('geolocation' in navigator) {
+      setAvailable(true);
+    }
+  }, [Available]) //第二引数が変わる度にuseEffectが発火される
+
+  const getPosition = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPositon({ latitude,longitude });
+    })
+  }
+
+  
 
 
+  //検索地の気象情報のデータ取得
+  const [fetchdata, setFetchData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  const textRef = useRef();
+  const onSubmit = (e) => {
+      e.preventDefault();
+      console.log(textRef.current.value);
+
+      const API_KEY = 'f7927c318eebf0ac033c6981f4624d57';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${textRef.current.value}&units=metric&appid=${API_KEY}`;
+
+      fetch(url)
+          .then((res) => {
+              return res.json();
+          })
+          .then((data) => {
+              console.log(data)
+              setFetchData(data);
+              setLoading(false);
+          })
+  }  
 
   return (
-    <div className="weatherInfo">
-        <div className='cityInfo'>
-            <h1 className='cityName'>tkyo</h1>
-            <p className='dateInfo'>2022 June 5 Sunday</p>
-            <img src='http://openweathermap.org/img/wn/10d@2x.png'/>
-            <div className='detailGroup'>
-                <p className='detailTemp'>最高温度:10</p>
-                <p className='detailTemp'>最低温度:2</p>
-                <p className='detailTemp'>湿度:3%</p>
-            </div>
-        </div>
-        <div className='cityTemp'>
-            <h2 className='temp'>3</h2> 
-        </div>
+    <div>
+        <header className='appForm'>
+            <i className="fa-light fa-location-arrow"></i>
+            <form className='searchForm' onSubmit={(e) => onSubmit(e)}>
+                <SearchIcon className='searchIcon'/>
+                <input className='searchInput' type='text' ref={textRef} placeholder="Search for location..."/>
+            </form>
+          </header>
+          <WeatherScreen data={fetchdata} load={loading} />
+        
     </div>
   )
-}
+  }
 
 export default App
--- src/components/WeatherScreen.jsx
+++ src/components/WeatherScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../App.css'
-import Form from '../components/Form'
+
 
 
 const WeatherScreen = ({ data, load }) => {
@@ -14,13 +14,13 @@ const WeatherScreen = ({ data, load }) => {
             <p className='dateInfo'>2022 June 5 Sunday</p>
             <img src='http://openweathermap.org/img/wn/10d@2x.png'/>
             <div className='detailGroup'>
-                <p className='detailTemp'>最高温度:{data.main.temp_max.toFixed()}</p>
-                <p className='detailTemp'>最低温度:{data.main.temp_min.toFixed()}</p>
+                <p className='detailTemp'>最高温度:{data.main.temp_max.toFixed()} ℃/℉</p>
+                <p className='detailTemp'>最低温度:{data.main.temp_min.toFixed()} ℃/℉</p>
                 <p className='detailTemp'>湿度:{data.main.humidity}%</p>
             </div>
         </div>
         <div className='cityTemp'>
-            <h2 className='temp'>{(data.main.temp).toFixed()}</h2> 
+            <h2 className='temp'>{(data.main.temp).toFixed()}<button>℃/℉</button></h2> 
         </div>
     </div>
     )
