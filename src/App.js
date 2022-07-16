2efab3172e60d8d7b1491f6fa6e5a0dbbb5b8040
nagizo <naginag0605@gmail.com>
2022-07-17 02:41:17 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,54 +1,160 @@
-import React, { useState, useRef } from 'react'
-import './App.css';
-import SearchIcon from '@mui/icons-material/Search';
-import WeatherScreen from '../src/components/WeatherScreen';
-import Card from '../src/components/Card';
-
-
+import React from 'react'
+import "./style.css" 
 
 const App = () => {
 
-  //API_KEY
-  
-  
-  //検索地の気象情報のデータ取得
-  const [fetchdata, setFetchData] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const MY_API_KEY = "f7927c318eebf0ac033c6981f4624d57";
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
 
-  const textRef = useRef();
-  const onSubmit = (e) => {
-      e.preventDefault();
-      console.log(textRef.current.value);
+    //現在地の天気情報を取得
+    navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        console.log(lat,long)
+    })
 
-      const API_KEY = 'f7927c318eebf0ac033c6981f4624d57';
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${textRef.current.value}&units=metric&appid=${API_KEY}`;
-     
-      fetch(url)
-          .then((res) => {
-              return res.json();
-          })
-          .then((data) => {
-              console.log(data)
-              setFetchData(data);
-              setLoading(false);
-          })      
-  }  
+    
 
-  return (
-    <div>
-        <header className='appForm'>
-            <i className="fa-light fa-location-arrow"></i>
-            <form className='searchForm' onSubmit={(e) => onSubmit(e)}>
-                <SearchIcon className='searchIcon'/>
-                <input className='searchInput' type='text' ref={textRef} placeholder="Search for location..."/>
-            </form>
-          </header>
-          <WeatherScreen data={fetchdata} load={loading} />
-          <Card />
-          
-          <Card />
-    </div>
-  )
-  }
+    return (
+        <>
+            <div className="seachInput">
+                <input placeholder="Search for location..."></input>
+            </div>
+
+            <div className="weatherScreen">
+                <div className="weatherInfo">
+                    <h1 className="cityName">London</h1>
+                    <p className="dateInfo">2022 June 5 Sunday</p>
+                    <span className="icon">☀️</span>
+                    <div className="tempGroup">
+                        <p>Max-Temp：１０</p>
+                        <p>Min-Temp</p>
+                        <p>Humidity：１０％</p>
+                    </div>
+                </div>
+
+                <div className="tempInfo">
+                    <p className="tempText">10</p>
+                    <div className="unitBtnGroup">
+                        <button className="unitBtn"><span className="unitColor">℃</span>/℉</button>
+                        <button className="unitBtn"><span className="unitColor">℉</span>/℃</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="cardGroup">
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/16 (月)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/17(火)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/18(水)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/19(木)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/20(金)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+
+                <div className="forecastCard">
+                    <div className="item1">
+                        <p className="forecastDate">7/21(土)</p>
+                    </div>
+                    <div className="item2">
+                        <p className="icon">☀️</p>
+                    </div>
+                    <div className="item3">
+                        <p>10</p>
+                        <button className="forecasUnitBtn"><span className="unitColor">℃</span>/℉</button>
+                    </div>
+                    <div className="item4">
+                        <p>10℃/℉</p>
+                        <p>10℃/℉</p>
+                        <p>10％</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
 
 export default App
